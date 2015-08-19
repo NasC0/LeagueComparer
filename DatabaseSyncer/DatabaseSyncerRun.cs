867497f7405a9932b81a3128603028e3b0ea36fe
NasC0 <Spirital@gmail.com>
2015-08-19 12:14:47 +0300

    Started working on the database syncer

---
+++ DatabaseSyncer/DatabaseSyncerRun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSyncer
+{
+    class DatabaseSyncerRun
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
